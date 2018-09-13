4221e37b20cabcbb726bcb8f5190eab57dc4cc4b
Charlie Parr <cparr@localhost.localdomain>
2018-09-13 11:32:50 -0800

    validation bash scripts for various DEM iterations

--- depth_dDEMs/clpx/uncorrected/compute_depth_w_final_arctic_adjusted_dem.sh
+++ depth_dDEMs/clpx/uncorrected/compute_depth_w_final_arctic_adjusted_dem.sh
@@ -12,6 +12,8 @@ gdal_calc.py -A ../../../DEMs/clpx/snow_surface/clpx_snow_on_096_2016.tif -B ../
 
 gdal_calc.py -A ../../../DEMs/clpx/snow_surface/clpx_snow_on_101_2017.tif -B ../../../DEMs/clpx/bare_earth/clpx_dem_final_minus_014m.tif --outfile=clpx_depth_from_arctic_adjusted_DEM_101_2017.tif --calc="A-B" --NoDataValue=-9999
 
+gdal_calc.py -A ../../../DEMs/clpx/snow_surface/clpx_snow_on_105_2018.tif -B ../../../DEMs/clpx/bare_earth/clpx_dem_final_minus_014m.tif --outfile=clpx_depth_from_arctic_adjusted_DEM_105_2018.tif --calc="A-B" --NoDataValue=-9999
+
 echo "Complete"
 
 exit 0
