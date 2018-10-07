87ecb2a67f5a4e1a4b37e9b8eee2ed50923daf3c
Charlie Parr <charlie@arcticfiredevelopment.com>
2018-10-06 20:39:52 -0800

    add params for hard cbar limits...messy. should create other functions for different geotiff information (depth, dem, etc..)

--- utils/plot_raster.py
+++ utils/plot_raster.py
@@ -13,6 +13,8 @@ parser.add_argument("-t", "--title", help="plot title")
 parser.add_argument("-o", "--output", help="output file name")
 parser.add_argument("-cmin", "--cmin", type=float, help="clip colorbar min")
 parser.add_argument("-cmax", "--cmax", type=float, help="clip colorbar max")
+parser.add_argument("-vmax", "--vmax", type=float, help="static colorbar max")
+parser.add_argument("-vmin", "--vmin", type=float, help="static colorbar min")
 
 args = parser.parse_args()
 
@@ -21,49 +23,63 @@ fig_x = int(10 * src.meta['width'] / src.meta['height'])
 fig_y = int(10 * src.meta['height'] / src.meta['width'])
 if fig_y > fig_x:
     fig_x += 2
-cmap = plt.get_cmap('viridis')
-arr = src.read(1)
-masked_arr = np.ma.masked_values(arr, src.nodata)
+cmap = plt.get_cmap('Spectral')
 
-vmin = np.min(masked_arr)
-vmax = np.max(masked_arr)
-mu = np.mean(masked_arr)
-sigma = np.std(masked_arr)
+#arr = src.read(1)
 
-textstr = '$\mu=%.2f$\n$\sigma=%.2f$\nmin = %.2f\nmax = %.2f' % (mu, sigma, vmin, vmax)
+
+# masked_arr = np.ma.masked_values(arr, src.nodata)
+#
+# vmin = np.min(masked_arr)
+# vmax = np.max(masked_arr)
+# mu = np.mean(masked_arr)
+# sigma = np.std(masked_arr)
+#
+# textstr = '$\mu=%.2f$\n$\sigma=%.2f$\nmin = %.2f\nmax = %.2f' % (mu, sigma, vmin, vmax)
 
 fig, ax = plt.subplots(figsize=(fig_x, fig_y))
+
 ax.set_title(args.title)
 ax.set_ylabel('UTM N Zone 6N')
 ax.set_xlabel('UTM E Zone 6N')
+#textstr = 'DRAFT'
 
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+#make this a cmd line arg
+
 # place a text box in upper left in axes coords
+# props = dict(boxstyle='round', facecolor='wheat', alpha=0.33)
+# if fig_y > fig_x:
+#     ax.text(0.05, 0.1, textstr, transform=ax.transAxes, fontsize=14,
+#     verticalalignment='top', bbox=props)
+# else:
+#     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+#     verticalalignment='top', bbox=props)
 
-if fig_y > fig_x:
-    ax.text(0.05, 0.1, textstr, transform=ax.transAxes, fontsize=14,
-            verticalalignment='top', bbox=props)
-else:
-    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
-            verticalalignment='top', bbox=props)
 
-if args.cmin:
-    if vmin > 0:
-        vmin *= (1 + args.cmin)
-    else:
-        vmin *= (1 - args.cmin)
-    if vmax > 0:
-        vmax *= (1 - args.cmax)
-    else:
-        vmax *= (1 + args.cmax)
-else:
-    # Do a 2% clip
-    vmin *= 1.02
-    vmax *= 0.98
+# if args.cmin:
+#     if vmin > 0:
+#         vmin *= (1 + args.cmin)
+#     else:
+#         vmin *= (1 - args.cmin)
+#     if vmax > 0:
+#         vmax *= (1 - args.cmax)
+#     else:
+#         vmax *= (1 + args.cmax)
+
+vmin = args.vmin
+vmax = args.vmax
+
+# else:
+#     # Do a 2% clip
+#     vmin *= 1.02
+#     vmax *= 0.98
 
 cmap.set_under('white')  # Color for values less than vmin
+#cmap.set_bad('black')
+
 show((src, 1), with_bounds=True, ax=ax, vmin=vmin, vmax=vmax, cmap=cmap)
+plt.setp( ax.xaxis.get_majorticklabels(), rotation=45 )
 PCM=ax.get_children()[-2]
 plt.colorbar(PCM, ax=ax)
 
-plt.savefig(args.output, dpi=300, bbox_inches='tight')
+plt.savefig(args.output, dpi=600, bbox_inches='tight')
