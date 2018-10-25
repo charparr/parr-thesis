0a62cbac0bdfd2f87ef0912b0d0a990b1dd1d86a
Charlie Parr <cparr@localhost.localdomain>
2018-10-25 10:28:54 -0800

    window size a user param. hillshade alt. 35

--- utils/make_topo_measures.sh
+++ utils/make_topo_measures.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-
-# vertical exaggeration factor. apply greater value for smaller scale / larger areas
-Z=1.3
+# vertical exaggeration factor, larger values can produce a better range of hillshade values
+Z=$1
+# window size for creating SD Dem, Slope Variance, etc.
+window_size=$2
 
 for filename in *.tif; do
 
   outname="${filename//.tif}"
-  echo $outname
-  #echo "../topo_measures/results/""$outname""/""$outname""_slope.tif"
 
   echo "Generating hillshade from $filename with sunlight angle at 45˚..."
-  gdaldem hillshade -of 'GTiff' -z $Z -az 45 $filename "$outname""_hillshade_az45.tif"
+  gdaldem hillshade -of 'GTiff' -z $Z -alt 35 -az 45 $filename "$outname""_hillshade_az45.tif"
 
   echo "Generating hillshade from $filename with sunlight angle at 135˚..."
-  gdaldem hillshade -of 'GTiff'  -z $Z -az 135 $filename "$outname""_hillshade_az135.tif"
+  gdaldem hillshade -of 'GTiff'  -z $Z -alt 35 -az 135 $filename "$outname""_hillshade_az135.tif"
 
   echo "Generating hillshade from $filename with sunlight angle at 225˚..."
-  gdaldem hillshade -of 'GTiff'  -z $Z -az 225 $filename "$outname""_hillshade_az225.tif"
+  gdaldem hillshade -of 'GTiff'  -z $Z -alt 35 -az 225 $filename "$outname""_hillshade_az225.tif"
 
   echo "Generating hillshade from $filename with sunlight angle at 315˚..."
-  gdaldem hillshade -of 'GTiff'  -z $Z -az 315 $filename "$outname""_hillshade_az315.tif"
+  gdaldem hillshade -of 'GTiff'  -z $Z -alt 35 -az 315 $filename "$outname""_hillshade_az315.tif"
 
   echo "Generating Slope from $filename..."
   gdaldem slope $filename "$outname""_slope.tif"
@@ -35,7 +34,7 @@ for filename in *.tif; do
   gdaldem roughness $filename "$outname""_roughness.tif"
 
   echo "Generating SD Elevation, SD Slope, Slope Variance, Area Ratio, Profile Curvature, and SD Profile Curvature..."
-  python /home/cparr/masters/utils/compute_roughness_metrics.py -d $filename -s "$outname""_slope.tif" -w 7
+  python /home/cparr/masters/utils/compute_roughness_metrics.py -d $filename -s "$outname""_slope.tif" -w $window_size
 
 done
 
