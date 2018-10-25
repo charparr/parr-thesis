f92db6d58ff953b03e993ef84eba2910a867c6db
Charlie Parr <cparr@localhost.localdomain>
2018-10-25 10:31:05 -0800

    100 estimators

--- random_forest_model/scripts/drift_classes.py
+++ random_forest_model/scripts/drift_classes.py
@@ -10,6 +10,7 @@ df = pd.read_csv('/home/cparr/workspace/pattern_similarity/results'
                  '/pixels_labeled_drift_or_not.csv')
 del df['Unnamed: 0']  # empty col, debug toolbox to cull this
 
+# X are the features. We need to remove depth, and the labels
 X = df.iloc[:, 1:-1]  # labels stored in last col and depth in first col
 X = X.values
 y = np.ravel(df.iloc[:, [-1]])
@@ -20,8 +21,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
                                                     random_state=0)
 pipe_rf = Pipeline([('sc', StandardScaler()),
                     ('rf', RandomForestClassifier(max_depth=10,
-                                                  n_estimators=50,
-                                                  random_state=1,
+                                                  n_estimators=100,
+                                                  oob_score=True,
                                                   n_jobs=-1))])
 
 
@@ -50,4 +51,3 @@ print('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 #                          cv=10,
 #                          n_jobs=-1)
 # print('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores1), np.std(scores1)))
-
