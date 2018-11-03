906ba5ee9b4267fc3e086007bb9f4f91779a5ba9
Charlie Parr <charlie@arcticfiredevelopment.com>
2018-11-02 16:28:27 -0800

    rm useless args

--- utils/plot_raster_add_stats.py
+++ utils/plot_raster_add_stats.py
@@ -1,18 +1,17 @@
+
 import rasterio
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from rasterio.plot import show
+plt.switch_backend('tkagg')
+print("Using backend: " + str(plt.get_backend()))
 
-np.seterr(all='ignore')
 
 parser = argparse.ArgumentParser(description='Utility to plot single rasters (GeoTiffs) with spatial coordinates with user supplied titles.')
-
 parser.add_argument("-r", "--raster", help="raster to plot")
-parser.add_argument("-t", "--title", help="plot title")
+parser.add_argument("-t", "--title", type=str, help="plot title")
 parser.add_argument("-o", "--output", help="output file name")
-#parser.add_argument("-cmin", "--cmin", type=float, help="clip colorbar min")
-#parser.add_argument("-cmax", "--cmax", type=float, help="clip colorbar max")
 parser.add_argument("-u", "--utm", type=str, help="utm zone number")
 parser.add_argument("-c", "--cmap", type=str, help="matplotlib colormap")
 parser.add_argument("-vmax", "--vmax", type=float, help="static colorbar max")
@@ -22,32 +21,32 @@ args = parser.parse_args()
 
 src = rasterio.open(args.raster)
 arr = src.read(1)
+
 masked_arr = np.ma.masked_values(arr, src.nodata)
 dmin = np.min(masked_arr)
 dmax = np.max(masked_arr)
 mu = np.mean(masked_arr)
 sigma = np.std(masked_arr)
 
+# Init. plot properties
 if args.cmap:
     cmap = plt.get_cmap(args.cmap)
 else:
     cmap = plt.get_cmap('Spectral')
 cmap.set_under('white')  # Color for values less than vmin
-
 xaxlabel = 'UTM E Zone ' + args.utm + ' N'
 yaxlabel = 'UTM N Zone ' + args.utm + ' N'
 fig_x = int(10 * src.meta['width'] / src.meta['height'])
 fig_y = int(10 * src.meta['height'] / src.meta['width'])
 if fig_y > fig_x:
     fig_x += 2
-
 textstr = '$\mu=%.2f$\n$\sigma=%.2f$\nmin = %.2f\nmax = %.2f' % (mu, sigma, dmin, dmax)
-fig, ax = plt.subplots(figsize=(fig_x, fig_y))
 
+# Create figure
+fig, ax = plt.subplots(figsize=(fig_x, fig_y))
 ax.set_title(args.title)
 ax.set_ylabel(xaxlabel)
 ax.set_xlabel(yaxlabel)
-
 # place a text box in upper left in axes coords
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.66)
 if fig_y > fig_x:
@@ -56,23 +55,6 @@ if fig_y > fig_x:
 else:
     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
     verticalalignment='top', bbox=props)
-
-# if args.cmin:
-#     if vmin > 0:
-#         vmin *= (1 + args.cmin)
-#     else:
-#         vmin *= (1 - args.cmin)
-#     if vmax > 0:
-#         vmax *= (1 - args.cmax)
-#     else:
-#         vmax *= (1 + args.cmax)
-# else:
-#     # Do a 2% clip
-#     vmin *= 1.02
-#     vmax *= 0.98
-#
-# vmin = args.vmin
-# vmax = args.vmax
 
 show((src, 1), with_bounds=True, ax=ax, vmin=args.vmin, vmax=args.vmax, cmap=cmap)
 plt.setp( ax.xaxis.get_majorticklabels(), rotation=45 )
