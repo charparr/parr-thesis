d3e51e4c466971f4c0f6805debefd1356c7bec2c
Charlie Parr <charlie@arcticfiredevelopment.com>
2018-11-02 16:29:24 -0800

    like nb, but doesnt work!

--- utils/plot_ortho.py
+++ utils/plot_ortho.py
@@ -1,26 +1,53 @@
 import rasterio
 import argparse
-import numpy as np
 import matplotlib.pyplot as plt
 from rasterio.plot import show
+plt.switch_backend('tkagg')
+print("Using backend: " + str(plt.get_backend()))
 
-parser = argparse.ArgumentParser(description='Utility to plot multiband orthomosaics with spatial coordinates and user supplied titles.')
+parser = argparse.ArgumentParser(description='Utility to plot multiband orthomosaics with spatial coordinates and user supplied titles. There are some weird quirks in the rasterio plot module...so to get an RGB image with spatial coordinates is janky...')
 
 parser.add_argument("-r", "--raster", help="raster to plot")
 parser.add_argument("-t", "--title", help="plot title")
+parser.add_argument("-u", "--utm", type=str, help="utm zone number")
 parser.add_argument("-o", "--output", help="output file name")
+parser.add_argument("-dpi", "--dpi", type=int, help="dpi for output fig")
 args = parser.parse_args()
 
 src = rasterio.open(args.raster)
-fig_x = int(10 * src.meta['width'] / src.meta['height'])
-fig_y = int(10 * src.meta['height'] / src.meta['width'])
+ortho = src.read()
+
+fig_x = int(src.meta['width'] / 1000)
+fig_y = int(src.meta['height'] / 1000)
+if fig_y > fig_x:
+    fig_x += 2
+
+print(fig_x, fig_y)
+xaxlabel = 'UTM E Zone ' + args.utm + ' N'
+yaxlabel = 'UTM N Zone ' + args.utm + ' N'
 
 fig, ax = plt.subplots(figsize=(fig_x, fig_y))
-ax.set_title(args.title)
-ax.set_ylabel('UTM N Zone 6N')
-ax.set_xlabel('UTM E Zone 6N')
+show(src, ax=ax)
 
-#arr = src.read(1)
-show((src), with_bounds=True, ax=ax)
+# Here comes the janky part
 
-plt.savefig(args.output, dpi=300, bbox_inches='tight')
+xtks = ax.xaxis.get_ticklabels()
+print(xtks)
+xtks1 = [str(int(str(x)[-8:-2]) - 1000) for x in xtks]
+print(xtks1)
+# ytks = ax.yaxis.get_ticklabels()
+# ytks1 = [str(int(str(y)[-9:-2]) + 2000 ) for y in ytks][::-1]
+#
+#
+# fig2, ax2 = plt.subplots(figsize=(fig_x, fig_y))
+# im = show(ortho, ax = ax2)
+# im.set_xticklabels(xtks1)
+# im.set_yticklabels(ytks1)
+#
+# ax2.set_title(args.title)
+# ax2.set_ylabel(xaxlabel)
+# ax2.set_xlabel(yaxlabel)
+
+#plt.setp( ax2.xaxis.get_majorticklabels(), rotation=45 )
+
+#plt.savefig(args.output, dpi=args.dpi, bbox_inches='tight')
