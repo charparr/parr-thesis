45c27e9caa7abeac5f21510ea4105946a9dd4e78
Charlie Parr <charlie@arcticfiredevelopment.com>
2018-11-30 16:35:16 -0900

    added a plot

--- drift_definition/drift_thresholds_by_study_area.py
+++ drift_definition/drift_thresholds_by_study_area.py
@@ -17,6 +17,10 @@ mean_df = df.copy().groupby('Study Area').mean()
 clpx_mean_thresh = mean_df.loc['CLPX']['Inflection Threshold']
 hv_mean_thresh = mean_df.loc['Happy Valley']['Inflection Threshold']
 
+b = df.groupby(['Study Area', 'Year']).mean()
+b['Year'] = [x[1] for x in b.index.values]
+b['Study Area'] = [x[0] for x in b.index.values]
+
 mean_df.to_csv('results/by_study_area/drift_threshold_by_study_area.csv')
 
 plt.figure(figsize=(16,10))
@@ -32,10 +36,15 @@ plt.savefig('figs/thresholds_by_study_area.png', dpi=300, bbox_inches='tight')
 
 sns.lmplot(x="CV", y="Inflection Threshold", hue='Study Area', data=df, height=16, aspect=1.5, truncate=True)
 plt.xlabel("CV of Snow Depth")
-plt.ylabel("Inflection Threshold (pct. of mean depth)")
+plt.ylabel("Threshold (pct. of mean depth)")
 plt.savefig('figs/thresholds_vs_cv.png', dpi=300, bbox_inches='tight')
 
 plt.figure(figsize=(16,10))
 sns.lineplot(x="Drift Threshold (pct. of mean depth)", y="Drift Volume-Area Difference (pct.)", hue="Study Area", style="Year", data=df)
 plt.ylim([0,25])
 plt.savefig('figs/thresholds_by_study_area_and_year.png', dpi=300, bbox_inches='tight')
+
+plt.figure(figsize=(16,10))
+sns.scatterplot(x="Year", y="Inflection Threshold", hue="Study Area", style='Study Area', alpha=0.66, data=b)
+plt.ylabel("Threshold (pct. of mean depth)")
+plt.savefig('figs/final_threshold_by_study_area_and_year.png', dpi=300, bbox_inches='tight')
