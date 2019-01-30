74c9dae19a7dfed814d13243c7d105a77138ef4e
Charlie Parr <charlie@arcticfiredevelopment.com>
2019-01-30 14:56:41 -0900

    consolidated to iqa metrics

--- similarity/similarity_tests/similarity_metrics.py
+++ similarity/similarity_tests/similarity_metrics.py
@@ -1,20 +1,21 @@
-#from similarity_tests.gradient_magnitude import compute_gms
-#from similarity_tests.feature_similarity import compute_fsim
-from .mean_square_error import compute_mse
-from .structural_similarity import compute_ssim
-from .complex_wavelet_ssim import compute_cw_ssim, cw_ssim
+#
+from .all_metrics_in_one import compute_mse
+from .all_metrics_in_one import compute_ssim
+from .all_metrics_in_one import cw_ssim
+from .all_metrics_in_one import compute_gms
+# from similarity_tests.feature_similarity import compute_fsim
 
 
 def compute_similarity(im1, im2):
-    """ Compute all similarity metrics and maps.
+    """ Compute all similarity metrics and maps and return a dictionary.
 
     """
 
     sim = dict()
-    sim['nrmse_val'], sim['nrmse_arr'] = compute_mse(im1, im2)
-    sim['ssim_val'], sim['ssim_arr'] = compute_ssim(im1, im2, 3)
-    sim['cwssim_val'], sim['cwssim_arr'] = cw_ssim(im1, im2, 30)
-    # results['gms_value'], results['gms_map'] = compute_gms(im1, im2)
-    # # results['fsim_value'], results['pc_max_map'] = compute_fsim(im1, im2)
-    #
+    sim['nrmse'], sim['nrmse_arr'] = compute_mse(im1, im2)
+    sim['ssim'], sim['ssim_arr'] = compute_ssim(im1, im2, 3)
+    sim['cwssim'], sim['cwssim_arr'] = cw_ssim(im1, im2, 30)
+    sim['gmsd'], sim['gms_arr'] = compute_gms(im1, im2)
+    # sim['fsim_value'], sim['pc_max_map'] = compute_fsim(im1, im2)
+
     return sim
