93c663c5657d88b06fda3c3388f493b3ea472f8b
Charlie Parr <charlie@arcticfiredevelopment.com>
2019-02-20 16:34:39 -0900

    index values reported on sim maps, fixed df creation bug, function to plot least similar pts on abs. depth diff map

--- similarity/similarity_tests/similarity_utils.py
+++ similarity/similarity_tests/similarity_utils.py
@@ -75,6 +75,52 @@ def create_pairs(d, compare_self=False):
     return rstr_pairs
 
 
+def results_to_dataframe(d, outpath):
+    """
+    Export dictionary of results to pandas DataFrame.
+
+    Can return two or three DataFrames depending on the input results. df
+    has all scores, rdf (optional) leaves out images compared with
+    themselves, and rdf assigns ranks of most to least similar because not
+    all metrics are on the same scale.
+    Args:
+    d (dict): dict of results for every comparisons
+    Returns:
+    df (DataFrame): all comparisons and scores
+    no_self (DataFrame): above but no self-comparisons (drop if nrmse == 0)
+    rdf (DataFrame): ranked scores
+    """
+    scores = []
+    metrics = []
+    pnames = []
+    for p in d:
+        pnames.append(p)
+        sc = []
+        for k in d[p]['results'].keys():
+            if type(d[p]['results'][k]) == np.float64:
+                sc.append(d[p]['results'][k])
+                if k not in metrics:
+                    metrics.append(k)
+        scores.append(sc)
+
+    df = pd.DataFrame(columns=metrics)
+    for result, name in zip(scores, pnames):
+        df.loc[name] = result
+    print(df.head(15))
+
+    rdf = pd.DataFrame()
+    rdf['NRMSE Rank'] = df['nrmse'].rank(ascending=False)
+    rdf['SSIM Rank'] = df['ssim'].rank(ascending=False)
+    rdf['CW-SSIM Rank'] = df['cwssim'].rank(ascending=False)
+    rdf['GMS Rank'] = df['gms'].rank(ascending=False)
+    rdf['Avg. Rank'] = rdf.mean(axis=1)
+    print(rdf.head(15))
+
+    df.to_csv(os.path.join(outpath) + 'iqa_results.csv')
+    rdf.to_csv(os.path.join(outpath) + 'iqa_ranks.csv')
+    return (df, rdf)
+
+
 def save_iqa_maps_to_geotiff(syr, pname, outpath):
     """
     Save the similarity metric maps to disk.
@@ -82,11 +128,11 @@ def save_iqa_maps_to_geotiff(syr, pname, outpath):
     Save all IQA similarity arrays to disk as GeoTIFF rasters using the
     metadata from the input snow map.
     Args:
-        syr (dict): Dictionary with comparison results from a single
-        pair of images
-        pname (str): the top level key of syr
-        Returns:
-            None - but writes .tif to disk
+    syr (dict): Dictionary with comparison results from a single
+    pair of images
+    pname (str): the top level key of syr
+    Returns:
+    None - but writes .tif to disk
     Raises:
     Exception: description
     """
@@ -103,72 +149,81 @@ def save_iqa_maps_to_geotiff(syr, pname, outpath):
                 else:
                     years.append(k)
                     profiles.append(syr[k]['profile'])
-    profile = profiles[0]
+                    profile = profiles[0]
 
-    pname = pname.replace(' ', '_')
+                    pname = pname.replace(' ', '_')
 
-    try:
-        pname_dir = os.path.join(outpath, pname)
-        os.makedirs(pname_dir)
-    except FileExistsError:
-        # directory already exists
-        pass
+                    try:
+                        pname_dir = os.path.join(outpath, pname)
+                        os.makedirs(pname_dir)
+                    except FileExistsError:
+                        # directory already exists
+                        pass
 
-    #outpath = os.path.join(os.path.join(pname_dir, pname))
+                        #outpath = os.path.join(os.path.join(pname_dir, pname))
 
-    for a, t in zip(arrs, iqa_names):
-        tname = os.path.join(pname_dir, (pname + '_' + t + '.tif'))
-        tname = tname.replace(' ', '_')
-        with rasterio.open(tname, 'w', **profile) as dst:
-            dst.write(a.astype('float32'), 1)
+                        for a, t in zip(arrs, iqa_names):
+                            tname = os.path.join(pname_dir, (pname + '_' + t + '.tif'))
+                            tname = tname.replace(' ', '_')
+                            with rasterio.open(tname, 'w', **profile) as dst:
+                                dst.write(a.astype('float32'), 1)
 
 
-def results_to_dataframe(d, outpath):
-        """
-        Export dictionary of results to pandas DataFrame.
+def plot_iqa_points_on_depth_diff_map(syr, pname, outpath):
+    """
+    Plot N most similar points on a depth difference map.abs
 
-        Can return two or three DataFrames depending on the input results. df
-        has all scores, rdf (optional) leaves out images compared with
-        themselves, and rdf assigns ranks of most to least similar because not
-        all metrics are on the same scale.
-        Args:
-        d (dict): dict of results for every comparisons
-        Returns:
-        df (DataFrame): all comparisons and scores
-        no_self (DataFrame): above but no self-comparisons (drop if nrmse == 0)
-        rdf (DataFrame): ranked scores
-        """
-        scores = []
-        metrics = []
-        pnames = []
-        for p in d:
-            print(p)
-            pnames.append(p)
-            sc = []
-            for k in d[p]['results'].keys():
-                if type(d[p]['results'][k]) == np.float64:
-                    sc.append(d[p]['results'][k])
-                    if k not in metrics:
-                        metrics.append(k)
-            scores.append(sc)
-        df = pd.DataFrame(columns=metrics)
-        for result, name in zip(scores, pnames):
-            df.loc[name] = result
-        print(df.head())
+    Create a plot where the lowest similarity scores (e.g. 10 lowest SSIM
+    pixels) are overlaid on a map of the absolute difference between two snow
+    depth maps (i.e. the two least similar snow depth patterns).
+    """
 
-        rdf = pd.DataFrame()
-        rdf['NRMSE Rank'] = df['nrmse'].rank(ascending=False)
-        print(rdf.head())
-        rdf['SSIM Rank'] = df['ssim'].rank(ascending=False)
-        rdf['CW-SSIM Rank'] = df['cwssim'].rank(ascending=False)
-        rdf['GMS Rank'] = df['gms'].rank(ascending=False)
-        rdf['Avg. Rank'] = rdf.mean(axis=1)
-        print(rdf.head())
+    arrs = []
+    iqa_names = []
+    years = []
+    depth_arrs = []
+    iqa_indicies = []
+    for k in syr.keys():
+        for j in syr[k].keys():
+            if isinstance(syr[k][j], (collections.Sequence, np.ndarray)):
+                if k[0].isalpha():
+                    arrs.append(syr[k][j])
+                    iqa_names.append(j)
+                else:
+                    years.append(k)
+                    depth_arrs.append(syr[k][j])
+    abs_delta = abs(depth_arrs[0] - depth_arrs[1])
 
-        dfs = (df, rdf)
-        df.to_csv(os.path.join(outpath) + 'iqa_results.csv')
-        rdf.to_csv(os.path.join(outpath) + 'iqa_ranks.csv')
-        return dfs
+    frac_size = 0.00001
+    for arr in arrs:
+        n_pts = round(frac_size * arr.size)
+        flat_indicies = np.argpartition(arr.ravel(),
+                                        n_pts - 1)[:n_pts]
+
+        row_indicies, col_indicies = np.unravel_index(flat_indicies,
+                                                      arr.shape)
+        pts = [i for i in zip(col_indicies, row_indicies)]
+        iqa_indicies.append(pts)
+
+    mrkrs = ['.m', '.c', '.y', '.g']
+    for p, a, m, n in zip(iqa_indicies, arr, mrkrs, iqa_names):
+        plt.figure()
+        ax = plt.imshow(abs_delta, vmin=0, vmax=2, cmap='coolwarm')
+        for i in p:
+            im = ax.axes.plot(i[0], i[1], m,
+                              markersize=8.0, markeredgecolor='k',
+                              alpha=0.5)
+        im[0].set_label(n[:-4])
+        plt.colorbar(orientation='horizontal')
+        plt.legend()
+        plt.title('Least Similar Points')
+        plt.suptitle('Basemap of Abs. Depth Difference [m]: ' + pname)
+        o = os.path.join(outpath, (pname + '_' + n[:-4] +
+                                         '_pts.png'))
+        o = o.replace(' ', '_')
+
+        plt.savefig(o, bbox_inches='tight', dpi=300)
+        plt.close()
 
 
 def plot_iqa_metric_maps(syr, pname, outpath):
@@ -190,11 +245,20 @@ def plot_iqa_metric_maps(syr, pname, outpath):
     """
     arrs = []
     titles = []
+    stat_str = []
+    index_vals = []
     for k in syr.keys():
         for j in syr[k].keys():
             if isinstance(syr[k][j], (collections.Sequence, np.ndarray)):
                 arrs.append(syr[k][j])
                 titles.append(j)
+                mu = np.nanmean(syr[k][j])
+                sig = np.nanstd(syr[k][j])
+                cv = round((sig/mu), 2)
+                textstr = '$\mu=%.2f$, $\sigma=%.2f$, CV=%.2f' % (mu, sig, cv)
+                stat_str.append(textstr)
+            elif isinstance(syr[k][j], (collections.Sequence, np.float64)):
+                index_vals.append(syr[k][j])
 
     titles = list(syr.keys()) + titles
     titles.remove('arr')
@@ -212,25 +276,27 @@ def plot_iqa_metric_maps(syr, pname, outpath):
         snow_dmaxs.append(np.nanmax(a))
     snowvmax = max(snow_dmaxs)
 
-    for t, a, ax in zip(pt[:2], arrs[:2], axes.flat[:2]):
+    for t, a, ax, st in zip(pt[:2], arrs[:2], axes.flat[:2],
+                            stat_str[:2]):
 
         im = ax.imshow(a, cmap='viridis',
                        interpolation='nearest',
                        vmin=0, vmax=snowvmax)
         ax.set_xticks([])
         ax.set_yticks([])
-        ax.set_title(t)
+        ax.set_title(t + '\n' + st)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical')
 
-    for t, a, ax in zip(pt[2:], arrs[2:], axes.flat[2:]):
+    for t, a, ax, st in zip(pt[2:], arrs[2:], axes.flat[2:],
+                            index_vals):
         im = ax.imshow(a, cmap='viridis',
                        interpolation='nearest',
                        vmin=0, vmax=1)
         ax.set_xticks([])
         ax.set_yticks([])
-        ax.set_title(t)
+        ax.set_title(t + ' = %.2f' % (st))
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical')
